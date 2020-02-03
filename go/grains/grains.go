1d7c1de60ba8f0ef297b4ec630325840cbcb09a9
Nijat <n.e.mahmudov@asan.gov.az>
2020-02-03 14:51:15 +0400

    Added descriptions to exported functions

--- go/grains/grains.go
+++ go/grains/grains.go
@@ -5,6 +5,7 @@ import (
 	"math"
 )
 
+// Square returns the amount of grains in a single square
 func Square(num int) (uint64, error) {
 	if num < 1 || num > 64 {
 		return 0, errors.New("out of range number, expected between 1 and 64 inclusively")
@@ -12,6 +13,7 @@ func Square(num int) (uint64, error) {
 	return uint64(math.Pow(2, float64(num-1))), nil
 }
 
+// Total calculates the amount of grains in all 64 squares
 func Total() uint64 {
 	total, _ := Square(65)
 	return total - 1
