3f56829ad9e6c8e671fd75ea38e0507a500c459e
Nijat Mahmudov <n.e.mahmudov@gmail.com>
2021-05-09 15:25:45 +0400

    refactor
    Change-Id: I3bfbb7abda9daba949fa14e78a21f3b009c637d7

--- go/clock/clock.go
+++ go/clock/clock.go
@@ -2,44 +2,31 @@ package clock
 
 import (
 	"fmt"
-	"strconv"
 )
+
+const minutesInHour = 60
+const minutesInADay = 24 * minutesInHour
 
 type Clock struct {
 	minutes int
 }
 
 func New(h, m int) Clock {
-	computed := normalize(h*60 + m)
-	return Clock{minutes: computed}
+	minutes := (h*minutesInHour + m) % minutesInADay
+	if minutes < 0 {
+		minutes += minutesInADay
+	}
+	return Clock{minutes: minutes}
 }
 
-func (c Clock) Add(n int) Clock {
-	c.minutes = normalize(c.minutes + n)
-	return c
+func (c Clock) Add(m int) Clock {
+	return New(0, c.minutes+m)
 }
 
-func (c Clock) Subtract(n int) Clock {
-	c.minutes = normalize(c.minutes - n)
-	return c
+func (c Clock) Subtract(m int) Clock {
+	return New(0, c.minutes-m)
 }
 
 func (c Clock) String() string {
-	c.minutes = normalize(c.minutes)
-	return fmt.Sprintf("%s:%s", format((c.minutes/60)%24), format(c.minutes%60))
-}
-
-func normalize(minutes int) int {
-	minutes %= 1440
-	if minutes < 0 {
-		minutes += 1440
-	}
-	return minutes
-}
-
-func format(n int) string {
-	if n < 10 {
-		return "0" + strconv.Itoa(n)
-	}
-	return strconv.Itoa(n)
+	return fmt.Sprintf("%02d:%02d", c.minutes/60, c.minutes%60)
 }
